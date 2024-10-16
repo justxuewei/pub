fa4f67c027eb78f60d7de83690a0a2b1ee1d444d
Xuewei Niu <justxuewei@apache.org>
2024-10-16 15:31:17 +0800

    Update install-rund.sh

--- kata-k8s/install-rund.sh
+++ kata-k8s/install-rund.sh
@@ -3,6 +3,7 @@
 set -x
 
 SHIM_NAME="containerd-shim-rund-v2"
+CLI_NAME="containerd-shim-rund-v2-cli"
 GUEST_IMG_NAME="kata-containers-rund.img"
 KERNEL_NAME="vmlinux-rund"
 SHARED_VOLUME="/vagrant"
@@ -25,3 +26,4 @@ fi
 cp -f "$SHARED_VOLUME/$SHIM_NAME" "/usr/local/bin/$SHIM_NAME"
 cp -f "$SHARED_VOLUME/$GUEST_IMG_NAME" "/root/$GUEST_IMG_NAME"
 cp -f "$SHARED_VOLUME/$KERNEL_NAME" "/root/$KERNEL_NAME"
+cp -f "$SHARED_VOLUME/$CLI_NAME" "/usr/local/bin/$CLI_NAME"
